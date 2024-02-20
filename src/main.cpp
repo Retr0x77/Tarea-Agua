c78376dd1088b88855eca289a910d95528b1cfb7
Retr0x77 <juli.olangua@gmail.com>
2024-02-20 11:10:10 -0500

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,141 +1,57 @@
-#include <iostream> 
-#include<conio.h> 
-#include <cctype> 
-#include <stdio.h> 
+#include <iostream>
+#include <cctype>
 
 using namespace std;
-class cisterna{
+
+class componente {  //Se crea una clase componente 
 private:
-	char estado;
-	public:
-		cisterna();
-		cisterna(char);
-		void setEstado(char);
-		char getEstado();
+    char estado; //La clase tiene como atributo el estado con una variable de tipo char
+public:
+	componente() {
+        estado = 'N'; 
+    }
+    componente(char Estado) {
+        estado = Estado;
+    }
+    void setEstado(char Estado) {
+        estado = Estado;
+    }
+    char getEstado() {
+        return estado;
+    }
 };
 
-cisterna::cisterna(){
-	estado='N';
+char Entrada(string mensaje){ //se crea una funcion para pedir los datos al usuario, la funcion recive el mensaje
+	string entrada;  //creamos una variable de tipo string para guardar la entrada (para evitar un error en el programa si el usuario ingresa mas de un caracter)
+	char caracter;   //creamos la variable caracter para guardar lo ingresado
+    do {
+        system("cls");
+        cout << mensaje;  //mostramos el mensaje        
+       getline(cin, entrada);  
+        caracter = toupper(entrada[0]);  //guardamos solo el 1er caracter de la entrada y lo convertimos en mayusculas (por si el usuario ingresa el caracter seguido de un espacio o alguna otra cosa)      
+    } while (cin.fail() || (caracter != 'S' && caracter != 'N')); //repetimos mientras no ingrese un caracter o lo ingresado se distinto de S o N
+    return caracter;  
 }
 
-cisterna::cisterna(char Estado){
-	estado=Estado;
+void Evaluar(componente C, componente B, componente T){ //Funcion para evaluar el estado de los componentes y mostrar el mensaje
+	system("cls");
+	if ((C.getEstado() == 'S' || T.getEstado() == 'N') && B.getEstado() == 'S') {
+    cout << "Apagar la bomba!!\n";
+} else if (C.getEstado() == 'S' && B.getEstado() == 'N') {
+    cout << "No se puede encender la bomba\n";
+} else if (C.getEstado() == 'N' && T.getEstado() == 'S' && B.getEstado() == 'N') {
+    cout << "Encender la bomba!!\n";
+} else {
+    cout << "No es necesario hacer nada\n";
+}
+}
+int main() {
+    componente Cisterna, Bomba, Tanque; //se crean los objetos
+    //Se llama a la funcion Entrada y se le asigna el valor que retorna al estado del objeto
+    Cisterna.setEstado(Entrada("La cisterna tiene poca agua? S/N: ")); 
+    Bomba.setEstado(Entrada("La bomba esta encendida? S/N: "));
+	Tanque.setEstado(Entrada("El tanque tiene poca agua? S/N: "));
+    Evaluar(Cisterna, Cisterna, Tanque);	//se llama a la funcion evaluar que nos dice lo que se debe hacer segun el estado de los objetos
+    system("pause"); //evita que el programa cierre sin que el usuario pueda ver lo que debe hacer
 }
 
-void cisterna::setEstado(char Estado){
-	estado=Estado;
-}
-
-char cisterna::getEstado(){
-	return estado;
-}
-
-class bomba{
-private:
-	char estado;
-	public:
-		bomba();
-		bomba(char);
-		void setEncendido(char);
-		char getEncendido();
-};
-
-bomba::bomba(){
-	estado='N';
-}
-
-bomba::bomba(char Estado){
-	estado=Estado;
-}
-
-void bomba::setEncendido(char Estado){
-	estado=Estado;
-}
-
-char bomba::getEncendido(){
-	return estado;
-}
-
-class tanque{
-private:
-	char estado;
-	public:
-		tanque();
-		tanque(char);
-		void setEstado(char);
-		char getEstado();
-};
-tanque::tanque(){
-	estado='E';
-}
-
-tanque::tanque(char Estado){
-	estado=Estado;
-}
-
-void tanque::setEstado(char Estado){
-	estado=Estado;
-}
-
-char tanque::getEstado(){
-	return estado;
-}
-
-
-int main(){
-    cisterna Cisterna;
-    bomba Bomba;
-    tanque Tanque;
-    
-    
-    string entrada; 
-    
-    char cisterna; 
-    char bomba;
-    char tanque;
-    
-     do{
-         system("cls") ;
-         cout<<"La cisterna tiene poca agua? S/N: ";      
-    cin>>entrada;
-     cin.ignore(1000, '\n');	
-    cisterna = entrada.at(0);
-    cisterna= toupper(cisterna);   
-       }while((cisterna!='S')&&(cisterna!='N')); 
-       Cisterna.setEstado(cisterna);  
-        do{
-         system("cls") ;
-         cout<<"La bomba esta encendida? S/N: ";
-   cin>>entrada;
-    cin.ignore(1000, '\n');
-    bomba = entrada.at(0);
-    bomba= toupper(bomba);    
-       }while((bomba!='S')&&(bomba!='N')); 
-        Bomba.setEncendido(bomba);
-       do{
-         system("cls") ;
-         cout<<"El tanque esta lleno o tiene poca agua? LLeno(F)/Poca Agua(E): "; //Fill (lleno) o Empty (Vacio)
-    cin>>entrada;
-         cin.ignore(1000, '\n');
-    tanque = entrada.at(0);
-    tanque= toupper(tanque);    
-       }while((tanque!='F')&&(tanque!='E')); 
-       Tanque.setEstado(tanque);
-        system("cls") ;
-       if((Cisterna.getEstado()=='S')&&(Bomba.getEncendido()=='S')){
-       	cout<<"Apagar la bomba!!";
-        }else if((Cisterna.getEstado()=='S')&&(Bomba.getEncendido()=='N')){
-	   	cout<<"No se puede encender la bomba";
-	   }else if((Tanque.getEstado()=='F')&&(Bomba.getEncendido()=='S')){
-	   	cout<<"Apagar la bomba!!";
-	   }else if((Cisterna.getEstado()=='N')&&(Tanque.getEstado()=='E')&&(Bomba.getEncendido()=='N')){
-	   	cout<<"Encender la bomba!!";
-	   }else if((Cisterna.getEstado()=='S')&&(Tanque.getEstado()=='F')&&(Bomba.getEncendido()=='N')){
-	   	cout<<"No es necesario hacer nada";
-	   }else if((Cisterna.getEstado()=='N')&&(Tanque.getEstado()=='E')&&(Bomba.getEncendido()=='S')){
-	   	cout<<"No es necesario hacer nada";
-	   }
-	   cout<<endl;
-	   cout<<endl;
-system("pause");
-}
